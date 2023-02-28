a23690a46bcd9810b84985db49154f0c6f3917ff
Badreddine <bb6848@ship.edu>
2023-02-28 10:40:32 -0500

    Cleaned up the Id on the switch

--- app/src/main/java/com/example/bedshakerswe415/Switch.java
+++ app/src/main/java/com/example/bedshakerswe415/Switch.java
@@ -8,13 +8,13 @@ import java.net.URL;
 public class Switch {
     private final int id;
 
-    public Switch(int id1) {
+    public Switch(int id) {
 
-        this.id = id1;
+        this.id = id;
     }
 
     public boolean TurnOn() throws MalformedURLException {
-        URL url = new URL("http://192.168.33.1/rpc/Switch.Toggle?id=0");
+        URL url = new URL("http://192.168.33.1/rpc/Switch.Toggle?id=" + id);
         try {
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
 
@@ -31,7 +31,7 @@ public class Switch {
     }
 
     public static void main(String[] args) throws MalformedURLException {
-        Switch switch1 = new Switch(1);
+        Switch switch1 = new Switch(0);
         switch1.TurnOn();
     }
 
