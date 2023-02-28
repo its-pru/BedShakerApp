5dc03b7e42b904b4d7ca2fcff6e2000d53dc1cb1
Tommy Prusak <tprusak01@gmail.com>
2023-02-27 22:26:17 -0500

    Added functionality to send http request to switch in java using a function

--- app/src/main/java/com/example/bedshakerswe415/Switch.java
+++ app/src/main/java/com/example/bedshakerswe415/Switch.java
@@ -8,17 +8,17 @@ import java.net.URL;
 public class Switch {
     private final int id;
 
-    public Switch(int id, int id1) {
+    public Switch(int id1) {
 
         this.id = id1;
     }
 
     public boolean TurnOn() throws MalformedURLException {
-        URL url = new URL("");
+        URL url = new URL("http://192.168.33.1/rpc/Switch.Toggle?id=0");
         try {
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
 
-            conn.setRequestMethod("Get");
+            //conn.setRequestMethod("Get");
             conn.connect();
             int responseCode = conn.getResponseCode();
             if(responseCode == 200) {
@@ -29,4 +29,13 @@ public class Switch {
         }
         return false;
     }
+
+    public static void main(String[] args) throws MalformedURLException {
+        Switch switch1 = new Switch(1);
+        switch1.TurnOn();
+    }
+
+
+
+
 }
