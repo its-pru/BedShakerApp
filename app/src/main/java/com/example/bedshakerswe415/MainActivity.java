c130f74fdac2f66ab10d77646adee4700935a87f
ep7481 <ep7481@cs.ship.edu>
2023-03-02 11:42:07 -0500

    Finished Iteration 1

--- app/src/main/java/com/example/bedshakerswe415/MainActivity.java
+++ app/src/main/java/com/example/bedshakerswe415/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.bedshakerswe415;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -13,25 +14,24 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        Switch switch1 = new Switch(0);
-//        try {
-//            switch1.TurnOn();
-//        } catch (IOException e) {
-//            throw new RuntimeException(e);
-//        }
-
     }
     boolean toggle = false;
-    public void toggle(View view) {
+    public void toggle(View view) throws IOException {
+        Switch switch1 = new Switch(0);
        TextView label = findViewById(R.id.lblToggle);
        toggle = !toggle;
        if(toggle) {
            label.setText("Bed Shaker ON");
+           switch1.TurnOn();
+
        }
        else {
            label.setText("Bed Shaker OFF");
+           switch1.TurnOn();
        }
     }
 }
