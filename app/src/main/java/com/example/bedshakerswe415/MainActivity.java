04f5bc59255308f7590bb4c2e16746b599c08acc
Hannah <01hgm25@gmail.com>
2023-03-30 10:20:35 -0400

    Set up navigation using Fragments to switch between pages.

--- app/src/main/java/com/example/bedshakerswe415/MainActivity.java
+++ app/src/main/java/com/example/bedshakerswe415/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.bedshakerswe415;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Context;
 import android.content.SharedPreferences;
@@ -14,6 +17,8 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.bedshakerswe415.databinding.ActivityMainBinding;
+
 import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
@@ -22,50 +27,92 @@ public class MainActivity extends AppCompatActivity {
     public static final String TEXT = "text";
     String ipCheck = "";
 
+    ActivityMainBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        // Binding helps switch between menu pages
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        // Please note that this is changed
+        // setContentView(R.layout.activity_main);
+        setContentView(binding.getRoot());
+        // Show home fragment when app just opened.
+        replaceFragment(new HomeFragment());
+        // Handles the logic for switching to a certain page, based on the
+        // item the user clicks on in the bottom navigation menu.
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.home:
+                    replaceFragment(new HomeFragment());
+                    break;
+                case R.id.wifi:
+                    replaceFragment(new WifiFragment());
+                    break;
+                case R.id.keywords:
+                    replaceFragment(new KeywordsFragment());
+                    break;
+                case R.id.contacts:
+                    replaceFragment(new ContactsFragment());
+                    break;
+            }
+            return true;
+        });
 
         SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, Context.MODE_PRIVATE);
         ipCheck = sharedPreferences.getString(TEXT, "");
         switch1.setPrivateIP(ipCheck);
 
-        if (ipCheck.equals("")) {
-            String IP;
-            try {
-                IP = switch1.getStatus();
-                System.out.println(IP);
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-            if (IP.equals("192.168.33.1")) {
-                try {
-                    switch1.setConfig();
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-                try {
-                    Thread.sleep(15000);
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-                try {
-                    IP = switch1.getStatus();
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-                saveMessage(IP);
-            }
-        }
+//        if (ipCheck.equals("")) {
+//            String IP;
+//            try {
+//                IP = switch1.getStatus();
+//                System.out.println(IP);
+//            } catch (IOException e) {
+//                throw new RuntimeException(e);
+//            }
+//            if (IP.equals("192.168.33.1")) {
+//                try {
+//                    switch1.setConfig();
+//                } catch (IOException e) {
+//                    throw new RuntimeException(e);
+//                }
+//                try {
+//                    Thread.sleep(15000);
+//                } catch (InterruptedException e) {
+//                    throw new RuntimeException(e);
+//                }
+//                try {
+//                    IP = switch1.getStatus();
+//                } catch (IOException e) {
+//                    throw new RuntimeException(e);
+//                }
+//                saveMessage(IP);
+//            }
+//        }
 
 
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && checkSelfPermission(Manifest.permission.RECEIVE_SMS) != PackageManager.PERMISSION_GRANTED) {
             requestPermissions(new String[]{Manifest.permission.RECEIVE_SMS}, 1000);
         }
     }
+
+    /**
+     * Replaces the mainFrameLayout item on the main_activity with the fragment
+     * passed to the function. A fragment acts like a page in the app, each
+     * has a different .xml file.
+     * @param fragment The fragment to change to.
+     */
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.mainFrameLayout, fragment);
+        fragmentTransaction.commit();
+    }
+
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
@@ -80,22 +127,21 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    boolean toggle = false;
-    public void toggle(View view) throws IOException {
-        toggle = !toggle;
-        if (toggle) {
-            TextView label = findViewById(R.id.lblToggle);
-            toggle = !toggle;
-            if (toggle) {
-                label.setText("Bed Shaker ON");
-                switch1.TurnOn();
-
-            } else {
-                label.setText("Bed Shaker OFF");
-                switch1.TurnOn();
-           }
-        }
-    }
+//    boolean toggle = false;
+//    public void toggle(View view) throws IOException {
+//        toggle = !toggle;
+//        if (toggle) {
+//            TextView awakeButton = findViewById(R.id.homeAwakeButton);
+//            toggle = !toggle;
+//            if (toggle) {
+//                awakeButton.setText("I'm Awake");
+//                //switch1.TurnOn();
+//            } else {
+//                awakeButton.setText("Shake It");
+//                //switch1.TurnOn();
+//           }
+//        }
+//    }
 
     private void saveMessage(String IP) {
         SharedPreferences sharedpreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
