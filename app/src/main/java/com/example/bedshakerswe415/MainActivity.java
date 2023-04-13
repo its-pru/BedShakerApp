d0fdc3260af22dc7a7f3bdac8a9f25fb904c0042
Tommy Prusak <tprusak01@gmail.com>
2023-04-13 10:54:51 -0400

    deleted not used stuff.

--- app/src/main/java/com/example/bedshakerswe415/MainActivity.java
+++ app/src/main/java/com/example/bedshakerswe415/MainActivity.java
@@ -26,8 +26,6 @@ import java.io.IOException;
 import java.lang.ref.WeakReference;
 
 public class MainActivity extends AppCompatActivity {
-    public static final String CHANNEL_ID = "autoStartServiceChannel";
-    public static final String CHANNEL_NAME = "Auto Start Service Channel";
     public static final String SHARED_PREFS = "shared_Prefs";
 
     Switch switch1;
@@ -42,30 +40,6 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-/*        if(!foregroundServiceRunning()) {
-            Intent serviceIntent = new Intent(this,
-                    MyForegroundService.class);
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                startForegroundService(serviceIntent);
-            }
-        }*/
-
-        //startService(new Intent(MainActivity.this, SmsProcessService.class));
-
-/*        Intent smsServiceIntent;
-        smsServiceIntent = new Intent(MainActivity.this, SmsProcessService.class);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            startForegroundService(smsServiceIntent);
-        }*/
-
-/*        if(!foregroundServiceRunning()) {
-            Intent serviceIntent = new Intent(this,
-                    SmsProcessService.class);
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                startForegroundService(serviceIntent);
-            }
-        }*/
 
         weakActivity = new WeakReference<>(MainActivity.this);
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
@@ -163,25 +137,4 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     }
-
-    /*public boolean foregroundServiceRunning(){
-        ActivityManager activityManager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
-        for(ActivityManager.RunningServiceInfo service: activityManager.getRunningServices(Integer.MAX_VALUE)) {
-            if(MyForegroundService.class.getName().equals(service.service.getClassName())) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public void startService(View v) {
-
-        Intent serviceIntent = new Intent(this, SmsProcessService.class);
-        serviceIntent.putExtra("inputExtra", "passing any text");
-        ContextCompat.startForegroundService(this, serviceIntent);
-    }
-    public void stopService(View v) {
-        Intent serviceIntent = new Intent(this, SmsProcessService.class);
-        stopService(serviceIntent);
-    }*/
 }
