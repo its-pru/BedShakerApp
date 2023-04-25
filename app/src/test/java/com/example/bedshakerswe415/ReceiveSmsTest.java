13d156327ba7c3d33f0f9f0dbf1ae211debbad53
ep7481 <ep7481@cs.ship.edu>
2023-04-25 11:41:17 -0400

    Working on mockito things

--- app/src/test/java/com/example/bedshakerswe415/ReceiveSmsTest.java
+++ app/src/test/java/com/example/bedshakerswe415/ReceiveSmsTest.java
@@ -1,16 +1,40 @@
 package com.example.bedshakerswe415;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 import android.app.Application;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
+
 import java.io.IOException;
 
-class ReceiveSmsTest {
+@RunWith(MockitoJUnitRunner.class)
+public class ReceiveSmsTest {
+    @Mock
     Context context = new Application().getApplicationContext();
+    @Mock
+    SharedPreferences sharedPreferences;
+    @Mock
+    SharedPreferences.Editor sharedPreferencesEditor;
+    Switch testSwitch;
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+        //  when(sharedPreferences.edit()).thenReturn(sharedPreferencesEditor);
+        //  when(sharedPreferencesEditor.putString(anyString(), anyString())).thenReturn(sharedPreferencesEditor);
+        testSwitch  = new Switch(0, sharedPreferences);
+    }
 
     private Intent createSmsIntent(String from, String body) {
         Intent smsIntent = new Intent(Intent.ACTION_VIEW);
@@ -23,11 +47,11 @@ class ReceiveSmsTest {
 
     // Test when msg contains keyword
     @Test
-    public void containKeywordTest() {
+    public void containKeywordTest() throws IOException {
         ReceiveSms rs = new ReceiveSms();
         rs.checkSender = false;
         rs.onReceive(context, createSmsIntent("99999999999", "WAKE UP"));
-        // TODO : Check if switch turned on
+        verify(testSwitch.TurnOn(), times(1));
     }
 
     // Test when contact is not in contact list
--- app/src/test/java/com/example/bedshakerswe415/switchApiTest.java
+++ app/src/test/java/com/example/bedshakerswe415/switchApiTest.java
@@ -1,8 +1,24 @@
 package com.example.bedshakerswe415;
 
+import static com.example.bedshakerswe415.Switch.TEXT;
+
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import android.content.Context;
+import android.content.SharedPreferences;
 
 import java.io.IOException;
 
@@ -11,12 +27,41 @@ import java.io.IOException;
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
+@RunWith(MockitoJUnitRunner.class)
 public class switchApiTest {
-    @Test
-    public void connectionTest() throws IOException {
-        //Switch testSwitch = new Switch(0, sharedpreferences);
-       // assertTrue(testSwitch.TurnOn());
-       // testSwitch.setConfig();
-        //testSwitch.getStatus();
+    @Mock
+    SharedPreferences sharedPreferences;
+    @Mock
+    SharedPreferences.Editor sharedPreferencesEditor;
+    Switch testSwitch;
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+        //  when(sharedPreferences.edit()).thenReturn(sharedPreferencesEditor);
+        //  when(sharedPreferencesEditor.putString(anyString(), anyString())).thenReturn(sharedPreferencesEditor);
+        testSwitch  = new Switch(0, sharedPreferences);
     }
+
+
+
+    /**
+     * Testing the return of IP from getStatus when on shelly switch
+     * Test1: no ip set on shelly
+     * Test2: ip is home wifi
+     * @throws IOException
+     */
+    @Test
+    public void getStatusOnShellyNetwork() throws IOException {
+        testSwitch.setConfig("Fios-V9QV4", "bond832sad5073copy");
+        assertEquals("192.168.1.172", testSwitch.getStatus());
+    }
+
+    @Test
+    public void testWifiCredentials() throws IOException {
+        testSwitch.setConfig("Fios-V9QV4", "bond832sad5073copy");
+        assertEquals("192.168.1.172", testSwitch.getStatus());
+        assertTrue(testSwitch.setConfig("Fios-V9QV4", "bond832sad5073copy"));
+    }
+
 }
