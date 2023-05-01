b88ca1e92cdbed6dcd3925469bb3169bd82e1fb2
ep7481 <ep7481@cs.ship.edu>
2023-05-01 12:28:10 -0400

    ReceiveSmsTest

--- app/src/test/java/com/example/bedshakerswe415/ReceiveSmsTest.java
+++ app/src/test/java/com/example/bedshakerswe415/ReceiveSmsTest.java
@@ -5,12 +5,35 @@ import static org.junit.jupiter.api.Assertions.*;
 import android.app.Application;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
+
 import java.io.IOException;
 
-class ReceiveSmsTest {
+
+@RunWith(MockitoJUnitRunner.class)
+public class ReceiveSmsTest {
+    @Mock
     Context context = new Application().getApplicationContext();
+    @Mock
+    SharedPreferences sharedPreferences;
+    @Mock
+    SharedPreferences.Editor sharedPreferencesEditor;
+    Switch testSwitch;
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+        //  when(sharedPreferences.edit()).thenReturn(sharedPreferencesEditor);
+        //  when(sharedPreferencesEditor.putString(anyString(), anyString())).thenReturn(sharedPreferencesEditor);
+        testSwitch  = new Switch(0, sharedPreferences);
+    }
 
     private Intent createSmsIntent(String from, String body) {
         Intent smsIntent = new Intent(Intent.ACTION_VIEW);
@@ -23,47 +46,46 @@ class ReceiveSmsTest {
 
     // Test when msg contains keyword
     @Test
-    public void containKeywordTest() {
+    public void containKeywordTest() throws IOException {
         ReceiveSms rs = new ReceiveSms();
         rs.checkSender = false;
         rs.onReceive(context, createSmsIntent("99999999999", "WAKE UP"));
-        // TODO : Check if switch turned on
+        assertTrue(testSwitch.TurnOn());
     }
 
     // Test when contact is not in contact list
     @Test
-    public void isContactTest() {
+    public void isContactTest() throws IOException {
         ReceiveSms rs = new ReceiveSms();
         rs.checkSender = true;
         rs.onReceive(context, createSmsIntent("17179453401", "WAKE UP"));
-
-        // TODO : Check if switch turned on
+        assertTrue(testSwitch.TurnOn());
     }
 
     // Test when contact is in contact list
     @Test
-    public void notContactTest() {
+    public void notContactTest() throws IOException {
         ReceiveSms rs = new ReceiveSms();
         rs.checkSender = true;
         rs.onReceive(context, createSmsIntent("99999999999", "WAKE UP"));
-        // TODO : Check if switch did NOT turn on
+        assertFalse(testSwitch.TurnOn());
     }
 
     // Test when you ignore the sender and contact is not in list
     @Test
-    public void ignoreSenderTest() {
+    public void ignoreSenderTest() throws IOException {
         ReceiveSms rs = new ReceiveSms();
         rs.checkSender = false;
         rs.onReceive(context, createSmsIntent("99999999999", "WAKE UP"));
-        // TODO : Check if switch turned on
+        assertTrue(testSwitch.TurnOn());
     }
 
 
     // Test when msg does not contain keyword
     @Test
-    public void notContainKeywordTest() {
+    public void notContainKeywordTest() throws IOException {
         ReceiveSms rs = new ReceiveSms();
         rs.onReceive(context, createSmsIntent("99999999999", "NOT A KEYWORD"));
-        // TODO : Check if switch did NOT turn on
+        assertFalse(testSwitch.TurnOn());
     }
 }
